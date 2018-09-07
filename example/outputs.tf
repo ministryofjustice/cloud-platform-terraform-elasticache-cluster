3a5b11c7d2557d3413c66be2bb8948d71c2b3c29
Razvan Cosma <36888774+razvan-moj@users.noreply.github.com>
2018-09-07 14:35:03 +0100

    Update outputs.tf

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,4 +1,3 @@
-output "cache_nodes" {
-  description = "List of node objects including id, address, port and availability_zone."
-  value       = "${module.example_team_ec_cluster.cache_nodes}"
+output "endpoint" {
+  value = "${join(":", list(aws_elasticache_cluster.redis.cache_nodes.0.address, aws_elasticache_cluster.redis.cache_nodes.0.port))}"
 }
