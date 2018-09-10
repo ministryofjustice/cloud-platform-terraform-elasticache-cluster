455af8347fce04181a252356dbc65757b162fe56
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-09-10 15:52:07 +0100

    correct ver

--- example/main.tf
+++ example/main.tf
@@ -3,8 +3,7 @@ provider "aws" {
 }
 
 module "example_team_ec_cluster" {
-  # source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=1.0"
-  source = "../"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=1.0"
 
   team_name              = "example-repo"
   ec_engine              = "redis"
