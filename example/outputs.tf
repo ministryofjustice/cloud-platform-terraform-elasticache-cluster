9ea64706e2facf6b4efa191564ce427797fd7fcf
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-09-10 14:16:16 +0100

    output all nodes

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,3 +1,8 @@
+output "cache_nodes" {
+  description = "List of node objects including id, address, port and availability_zone."
+  value       = "${aws_elasticache_cluster.ec_cluster.cache_nodes}"
+}
+
 output "endpoint" {
   value = "${join(":", list(module.example_team_ec_cluster.0.address, module.example_team_ec_cluster.0.port))}"
 }
