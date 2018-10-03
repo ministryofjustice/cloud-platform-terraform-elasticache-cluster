23679c8bf596d62f57aa2be972ce63daaa4206d0
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-10-03 16:22:50 +0100

    Update and cleanup example
    The example now is specific to cloud-platform and is to be the source for documentation.

--- example/main.tf
+++ example/main.tf
@@ -1,16 +1,42 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+// The following two variables are provided at runtime by the pipeline.
+
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
+
+// Make sure that you use the latest version of the module by changing the
+// `ref=` value in the `source` attribute to the latest version listed on the
+// releases page of this repository.
 module "example_team_ec_cluster" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=2.0"
 
-  cluster_name           = "cloud-platform-live-0"
-  cluster_state_bucket   = "moj-cp-k8s-investigation-platform-terraform"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
   team_name              = "example-repo"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
-  node_type              = "cache.m3.medium"
-  number_cache_clusters  = "2"
   business-unit          = "example-bu"
   application            = "exampleapp"
   is-production          = "false"
   environment-name       = "development"
   infrastructure-support = "example-team@digtal.justice.gov.uk"
+}
+
+resource "kubernetes_secret" "elasticache" {
+  metadata {
+    name      = "example-team-ec-cluster-output"
+    namespace = "my-namespace"
+  }
+
+  data {
+    primary_endpoint_address = "${module.example_team_ec_cluster.primary_endpoint_address}"
+    member_clusters          = "${module.example_team_ec_cluster.member_clusters}"
+    auth_token               = "${module.example_team_ec_cluster.auth_token}"
+  }
 }
