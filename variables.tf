5a0317a28d783f8294dbbfbca96f38795c0fd3e1
jakemulley <jakemulley@users.noreply.github.com>
2023-07-04 15:24:39 +0100

    Remove defaults from core configuration variables

--- variables.tf
+++ variables.tf
@@ -7,27 +7,24 @@ variable "vpc_name" {
 }
 
 variable "engine_version" {
-  description = "The engine version that your ElastiCache Cluster will use. This will differ between the use of 'redis' or 'memcached'. The default is '5.0.6' with redis being the assumed engine."
+  description = "Engine version (e.g. 7.0)"
   type        = string
-  default     = "5.0.6"
 }
 
 variable "parameter_group_name" {
-  description = "Name of the parameter group to associate with this cache cluster. Again this will differ between the use of 'redis' or 'memcached' and your engine version. The default is 'default.redis5.0'."
+  description = "Name of the parameter group aligned with the version specified in engine_version (e.g. default.redis7)"
   type        = string
-  default     = "default.redis5.0"
-}
-
-variable "number_cache_clusters" {
-  description = "The number of cache clusters (primary and replicas) this replication group will have. Default is 2"
-  type        = string
-  default     = "2"
 }
 
 variable "node_type" {
-  description = "The cache node type for your cluster. The next size up is cache.m4.large "
+  description = "Instance class to be used"
   type        = string
-  default     = "cache.t2.medium"
+}
+
+variable "number_cache_clusters" {
+  description = "Number of cache clusters (primary and replicas) this replication group will have"
+  type        = string
+  default     = "2"
 }
 
 variable "snapshot_window" {
